247a90c9b14f79a826c8149ee1fcbb285e40deaa
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2012-09-16 17:47:04 +0300

    queue push tasks to ordered array

--- lib/queue.rb
+++ lib/queue.rb
@@ -6,7 +6,7 @@ class Queue
   end
 
   def push(task)
-    @q << task
+    @q.insert(@q.index { |item| item.finish_time >= task.finish_time } || @q.size, task)
     self
   end
 end
--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -5,12 +5,28 @@ Queue.class_eval do
   def size
     @q.size
   end
+
+  def native
+    @q
+  end
 end
 
 describe Queue do
-  it "should add task to queue" do
-    q = Queue.new
-    q.push(Task.new(:finish_time => Time.now, :description => "First")).size.should eq(1)
-    q.push(Task.new(:finish_time => Time.now, :description => "Second")).size.should eq(2)
+  describe "#push" do
+    it "should add task to queue" do
+      q = Queue.new
+      q.push(Task.new(:finish_time => Time.now, :description => "First")).size.should eq(1)
+      q.push(Task.new(:finish_time => Time.now, :description => "Second")).size.should eq(2)
+    end
+
+    it "should order queue by finish time" do
+      q = Queue.new
+      t1 = Task.new(:finish_time => Time.now, :description => "Second")
+      t2 = Task.new(:finish_time => Time.now + 2592000, :description => "Fourth")
+      t3 = Task.new(:finish_time => Time.now, :description => "Third")
+      t4 = Task.new(:finish_time => Time.now - 2592000, :description => "First")
+      q.push(t1).push(t2).push(t3).push(t4)
+      q.native.should eq([t1, t2, t3, t4].sort { |a, b| a.finish_time <=> b.finish_time })
+    end
   end
 end
